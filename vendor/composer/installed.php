b479377fcf03284261d41565510337c6e7c12cbc
白泽 <741499810@qq.com>
2021-06-02 09:56:40 +0800

    1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '66bf0b3f0961cdc19d9cff7ae1b5fdcd3afb400d',
+    'name' => 'baize/pay',
+  ),
+  'versions' => 
+  array (
+    'baize/pay' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '66bf0b3f0961cdc19d9cff7ae1b5fdcd3afb400d',
+    ),
+  ),
+);
